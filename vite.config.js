359778a3b17e91f614602d6c405d98f02fe65ae6
RomainJ <romain.jacquesson@gmail.com>
2026-08-03 21:47:31 +0200

    mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/",
-  server: {
-    open: true,
-  },
-});
+  base: mode === "github-pages" ? "/Socratea/" : "/",
+}));
